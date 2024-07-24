a45494d514d1aff20157c923c8e07edc9567122e
SURAJJAIS9580 <surajjaiswal5857@gmail.com>
2024-07-24 22:34:28 +0530

    Conditional Statement in java

---
+++ Javaprogram/src/ConditionalStatement/ConditionalStatement.java
@@ -0,0 +1,18 @@
+package ConditionalStatement;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        //check the user is valid for ride vehicles
+        System.out.println("Enter user age: ");
+        Scanner scanner=new Scanner(System.in);
+        int myAge = scanner.nextInt();
+        if (myAge > 18){
+            System.out.println("You'r valid for ride vehicles");
+        }
+        else {
+            System.out.println("You'r not valid for ride vehicles");
+        }
+    }
+}
