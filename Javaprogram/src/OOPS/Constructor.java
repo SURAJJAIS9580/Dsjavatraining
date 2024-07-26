53c7b78995bdae46867956d92f8d97844ded3ef2
SURAJJAIS9580 <surajjaiswal5857@gmail.com>
2024-07-26 08:04:08 +0530

    OOPs in Java

---
+++ Javaprogram/src/OOPS/Constructor.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Constructor {
+    public static void main(String[] args) {
+        //call the class instance using object
+        StudentProfile studentProfile = new TrainerProfile();
+
+    }
+}
+
+class TrainerProfile{
+    //create a constructor and print name
+  TrainerProfile(){
+        System.out.println("Im suraj jaiswal is student");
+    }
+}
