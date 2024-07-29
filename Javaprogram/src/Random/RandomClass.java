dec482adcfd55634ea85ca4b3cd12241a4670bec
SURAJJAIS9580 <surajjaiswal5857@gmail.com>
2024-07-30 00:17:34 +0530

    Random Class in java

--- Javaprogram/src/Random/RandomClass.java
+++ Javaprogram/src/Random/RandomClass.java
@@ -10,11 +10,13 @@ public class RandomClass {
         float floatNo = random.nextFloat();
         double doubleNo = random.nextDouble();
         long longNo = random.nextLong();
+
         boolean booleanNo = random.nextBoolean();
+
         System.out.println("Int no is:"+ intNo);
         System.out.println("Float no is"+ floatNo);
         System.out.println("Double no is"+ doubleNo);
-        System.out.println("Long no is"+longNo);
-        System.out.println("boolean no is"+booleanNo);
+        System.out.println("Long no is"+ longNo);
+        System.out.println("boolean no is"+ booleanNo);
     }
 }
